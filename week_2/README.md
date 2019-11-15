2361e346656f7d7a8d22a9fa2db90c1bdbfd3a4b
Swen Mulderij <swenmulderij@gmail.com>
2019-11-15 11:08:28 +0100

    les twee after

--- week_2/README.md
+++ week_2/README.md
@@ -1,7 +1,7 @@
 # Week 2
 
 ## More datatypes
-- Last lesson: Int/FLoats & Strings
+- Last lesson: Int/Floats & Strings, any questions?
 - Bools (What type is '5 > 4'?)
 - Boolean logic
 - Variables
@@ -11,4 +11,7 @@
 ## Resources
 - For Boolean logic the truth tables on page 129 of LPTHW are very usefull.
 - The python tutorial covers all the basic datatypes here.
+- [PEP8](https://www.python.org/dev/peps/pep-0008/#indentation)
+## Deelnemers die er niet waren
 
+- Tom 
---
+++ week_2/tweede.py
@@ -0,0 +1,61 @@
+# Integer (natuurlijk getal), Float (gebroken getallen), String (teksten)
+
+# print(5 / "swen")
+
+# print(5 /2 )
+# print(str(5) / 2)
+
+# Boolean -> True of False
+# print(bool(""))
+# print(None == "")
+
+# b1 -> Erik loopt buiten : True
+# b2 -> Het regent : False
+
+
+# Boolean logic:
+# b1 EN b2  -> FALSE
+# b1 EN b1 -> TRUE
+# b1 OF b2 -> TRUE
+# NIET(b1) -> FALSE
+# b2 OF b2 -> FALSE
+# b1 OF b1 -> TRUE
+
+# print(True and False) # False
+# print(True and True) # True
+# # print(True or False) # True
+# print(not(True)) # False
+# print(False or False) # False
+# print(True or True) # True
+
+# print(not(5 != 6))
+
+# 5 == 6
+# 4 != 5
+
+# print(7/2)
+
+# Variabele
+# f(brood) = brood^2 + 4
+
+# hp = 5 # Assign the value 5 to the identifier 'hp'
+# hp = hp + 1
+# damage = 4
+# hp = hp - damage
+# print(hp)
+# hp = hp + 2
+# print(hp)
+# assign = retrieve
+
+moet_werken = True
+persoon = "Erik"
+verdieping = 12
+
+if moet_werken:
+    
+    print("Hallo, " + persoon + " je moet naar verdieping: " + str(verdieping))
+
+else:
+    print("Thuisblijven")
+
+print("Programma klaar")
